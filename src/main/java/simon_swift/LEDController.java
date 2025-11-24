5781643a7a91674d5316e0f5c9f0bb94dc483b9d
Nafiul Alam <138951543+Gl1tchzz@users.noreply.github.com>
2025-11-24 22:21:19 +0000

    Implement color mapping methods in LEDController
    Added methods to map colors to LEDs and RGB values.

--- src/main/java/simon_swift/LEDController.java
+++ src/main/java/simon_swift/LEDController.java
@@ -1,20 +1,37 @@
 package simon_swift;
 
 import swiftbot.SwiftBotAPI;
+import swiftbot.Underlight;
 
 public class LEDController {
 
     private SwiftBotAPI api;
 
     public LEDController(SwiftBotAPI api) {
-
+        this.api = api;
     }
 
-    public void showColour(GameColour colour) {
-        // light up LED
+    // Mapping colours to LEDs
+    private Underlight getUnderlightForColour(GameColour colour) {
+        
     }
 
+    // Mapping colours to RGB values
+    private int[] getRgbForColour(GameColour colour) {
+        
+    }
+
+    public void showColour(GameColour colour, int duration) {
+        
+        // Display the colour
+       
+
+        // Turn off after duration cooldown
+        
+    }
+
+    // Clear all lights
     public void clearLights() {
-        // turn off lights
+      
     }
 }
