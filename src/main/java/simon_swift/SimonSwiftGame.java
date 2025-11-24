1b1c70311bed04a8ee62a254f551c2f349b9039c
Nafiul Alam <138951543+Gl1tchzz@users.noreply.github.com>
2025-11-24 22:25:28 +0000

    Refactor SimonSwiftGame to improve game logic

--- src/main/java/simon_swift/SimonSwiftGame.java
+++ src/main/java/simon_swift/SimonSwiftGame.java
@@ -1,5 +1,8 @@
 package simon_swift;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import swiftbot.SwiftBotAPI;
 
 public class SimonSwiftGame {
@@ -10,24 +13,84 @@ public class SimonSwiftGame {
     private ButtonInputHandler buttonInputHandler;
     private SequenceManager sequenceManager;
     private CelebrationController celebrationController;
+    private SequenceValidator sequenceValidator;
 
     private int score;
     private int round;
-    private GameState gameState;
 
     public SimonSwiftGame() {
-        // initialise objects here
+        api = SwiftBotAPI.INSTANCE;
+
+        ledController = new LEDController(api);
+        buttonInputHandler = new ButtonInputHandler(api);
+        sequenceManager = new SequenceManager();
+        celebrationController = new CelebrationController(api);
+        sequenceValidator = new SequenceValidator();
+
+        score = 0;
+        round = 0;
     }
 
     public void startGame() {
-        // start the game loop
+        System.out.println("Welcome to Simon Swift!");
+        System.out.println("Watch the colours, then repeat them using the buttons A, B, X, Y.");
+        System.out.println();
+
+        sequenceManager.resetSequence();
+        score = 0;
+        round = 0;
+
+        boolean playing = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (playing) {
+            boolean sucsess = playRound();
+
+            if (!sucsess) {
+                System.out.println("Game Over!");
+                playing = false;
+            } else {
+                score++;
+
+                if (round % 5 == 0) {
+                    System.out.println("You reached round " + round + ". Do you want to continue? (y/n): ");
+                    String answer = scanner.nextLine();
+
+                    if (answer.equalsIgnoreCase("n")) {
+                        System.out.println("Thanks for playing! Your final score is: " + score);
+                        playing = false;
+                    }
+                }
+            }
+        }
+        endGame();
+        scanner.close();
     }
 
-    public void playRound() {
-        // play one round
+    public boolean playRound() {
+        //Increment Round
+        round++;
+
+        //Display Round Info
+        System.out.println("----------------------");
+        System.out.println("Round: " + round);
+        System.out.println("Current score: " + score);
+        System.out.println("----------------------");
+
+        // Generate the Colour Sequence
+        
+
+        // Show the sequence to the player
+       
+
+        // Get player input
+
+        
+        // Validate the input
+        
     }
 
     public void endGame() {
-        // finish the game
+        
     }
 }
