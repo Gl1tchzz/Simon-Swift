3317502c8070dde489f24d6fce39700c259e9e40
Nafiul Alam <nafiulalam26@gmail.com>
2025-11-25 12:38:48 +0000

    Improved Terminal Styles

--- src/main/java/simon_swift/ButtonInputHandler.java
+++ src/main/java/simon_swift/ButtonInputHandler.java
@@ -23,25 +23,25 @@ public class ButtonInputHandler {
         // A -> RED
         api.enableButton(Button.A, () -> {
             lastColourPressed = GameColour.RED;
-            System.out.println("Button A (RED) pressed");
+            //System.out.println("Button A (RED) pressed");
         });
 
         // B -> GREEN
         api.enableButton(Button.B, () -> {
             lastColourPressed = GameColour.GREEN;
-            System.out.println("Button B (GREEN) pressed");
+            //System.out.println("Button B (GREEN) pressed");
         });
 
         // X -> BLUE
         api.enableButton(Button.X, () -> {
             lastColourPressed = GameColour.BLUE;
-            System.out.println("Button X (BLUE) pressed");
+            //System.out.println("Button X (BLUE) pressed");
         });
 
         // Y -> YELLOW
         api.enableButton(Button.Y, () -> {
             lastColourPressed = GameColour.YELLOW;
-            System.out.println("Button Y (YELLOW) pressed");
+            //System.out.println("Button Y (YELLOW) pressed");
         });
     }
 
